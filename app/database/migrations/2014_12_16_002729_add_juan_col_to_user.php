fc5e2b06f420126cd2afa7469c253a55913c5b07
juan2ramos <juan2ramos@gmail.com>
2014-12-18 21:20:15 -0500

    PhpStorm

--- app/database/migrations/2014_12_16_002729_add_juan_col_to_user.php
+++ app/database/migrations/2014_12_16_002729_add_juan_col_to_user.php
@@ -3,28 +3,29 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddJuanColToUser extends Migration {
+class AddJuanColToUser extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-        Schema::table('user',function($table){
-            $table->string('notif',10);
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function ($table) {
+            $table->string('notif', 10);
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
 
 }
